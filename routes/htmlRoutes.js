d7c636a8d7d753b718b7c6c55282e76e65987f85
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-29 15:29:53 -0700

    initial commit. Prepared the serve.js file with routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = function (app){
+    
+    app.get('/notes', function(req, res) {
+        response.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    app.get('*', function(req, res) {
+        response.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
